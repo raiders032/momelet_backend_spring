2815a8e3016d9d68eb2c24c8d80c77a1ee848a44
raiders032 <nameks@naver.com>
2021-02-23 14:20:12 +0900

    apple-login

---
+++ src/main/java/com/swm/sprint1/apple/model/AppsResponse.java
@@ -0,0 +1,15 @@
+package com.swm.sprint1.apple.model;
+
+public class AppsResponse {
+
+    private String payload;
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+}
