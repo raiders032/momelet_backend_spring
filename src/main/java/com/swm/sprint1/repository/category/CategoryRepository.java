81e08614b64bd90f4d05ff73db31da482e2a4eb0
raiders032 <nameks@naver.com>
2021-03-02 18:34:33 +0900

    refactor CategoryRepository

--- src/main/java/com/swm/sprint1/repository/category/CategoryRepository.java
+++ src/main/java/com/swm/sprint1/repository/category/CategoryRepository.java
@@ -4,5 +4,4 @@ import com.swm.sprint1.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,Long>, CategoryRepositoryCustom {
-    Category findByName(String name);
 }
--- src/main/java/com/swm/sprint1/repository/category/CategoryRepositoryCustom.java
+++ src/main/java/com/swm/sprint1/repository/category/CategoryRepositoryCustom.java
@@ -1,6 +1,5 @@
 package com.swm.sprint1.repository.category;
 
-import com.querydsl.core.Tuple;
 import com.swm.sprint1.domain.Category;
 
 import java.util.List;
