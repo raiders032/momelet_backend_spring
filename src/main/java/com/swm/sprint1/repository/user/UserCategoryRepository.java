53ee257e4f41f314f2251d12bd9cc7a9b79d088f
raiders032 <nameks@naver.com>
2021-03-02 19:11:57 +0900

    refactor UserRepository

--- src/main/java/com/swm/sprint1/repository/user/UserCategoryRepository.java
+++ src/main/java/com/swm/sprint1/repository/user/UserCategoryRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserCategoryRepository extends JpaRepository<UserCategory,Long>, UserCategoryRepositoryCustom {
-    List<UserCategory> findByUserId(Long id);
 }
--- src/main/java/com/swm/sprint1/repository/user/UserRepositoryCustom.java
+++ src/main/java/com/swm/sprint1/repository/user/UserRepositoryCustom.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepositoryCustom {
-    List<User> findAllCustom();
 
     Optional<User> findUserWithUserCategory(Long id);
 }
