6f9745f3aaedf54d58c1df2a731dbde429da7870
raiders032 <nameks@naver.com>
2021-04-12 21:02:59 +0900

    Exception 정리

--- src/main/java/com/swm/sprint1/exception/NotSupportedExtension.java
+++ src/main/java/com/swm/sprint1/exception/NotSupportedExtension.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotSupportedExtension extends RuntimeException {
+
     public NotSupportedExtension(String message) {
         super(message);
     }
 
-    public NotSupportedExtension(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
